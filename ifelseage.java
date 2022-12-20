ac7157f8c6eef24a59d87bfa09a1a661431c9b5b
viramodi <patelviraj695@gmail.com>
2022-12-20 23:05:23 +0000

    first commit

---
+++ ifelseage.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class ifelseage {
+    public static void main(String[] args) {
+        int a=18;
+        if (a>18) {
+            System.out.println("a is smaller value");
+        }else {
+            System.out.println("a is bigger value");
+        }
+    }
+}
