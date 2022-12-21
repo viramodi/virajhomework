7a510f72d83d9ceadd825fa1fc52bcd45899497c
viramodi <patelviraj695@gmail.com>
2022-12-21 19:51:26 +0000

    update2

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int d = 10;
+        if (d % 3 == 0) {
+            System.out.println("d is even");
+        }else
+            System.out.println("d is odd");
+    }
+}
---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int e=20;
+        int f=20;
+        System.out.println(e+f);
+        System.out.println(e-f);
+        System.out.println(e*f);
+        System.out.println(e/f);
+
+    }
+}
