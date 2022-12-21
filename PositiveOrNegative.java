c573afe976be035f17a718c06423cbf4fc416355
viramodi <patelviraj695@gmail.com>
2022-12-21 18:57:20 +0000

    Updated

---
+++ PositiveOrNegative.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        int b=5;
+        if (b>5) {
+            System.out.println("b is a negative");
+        }else {
+            System.out.println("b is positive");
+        }
+    }
+}
