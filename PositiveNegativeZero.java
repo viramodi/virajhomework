a37b3db671f7fd5fa52f32c6b7273c938f17e845
viramodi <patelviraj695@gmail.com>
2022-12-21 19:22:24 +0000

    update1

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int c=0;
+        if (c<0) {
+            System.out.println("c is negative");
+        }else if (c>0) {
+            System.out.println("c is a positive");
+        }
+        else
+            System.out.println("c is a zero");
+    }
+}
